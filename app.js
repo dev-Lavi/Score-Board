7343955887239a442d98b869498dad71606e6bb5
Lavi sharma <lavipandit123@gmail.com>
2025-04-22 22:01:21 +0530

    added rate limiter

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ connectDB()
   .then(() => {
 
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, () => console.log(`this Server running on port ${PORT}`));
   })
   .catch((err) => {
     console.error('Error connecting to the database:', err);
